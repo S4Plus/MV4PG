d7be1cdfb8ebc67ad3783bb7217c81468b1ac2c9
seijiang <angels@mail.ustc.edu.cn>
2024-11-12 09:24:02 +0800

    tugraph test

--- tugraph_test/tugraph_prepare.sh
+++ tugraph_test/tugraph_prepare.sh
@@ -2,13 +2,13 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR"
 
 # compile
-cd "$SCRIPT_DIR/tugraph_db_without_views"
-mkdir build & cd build
+cd ./tugraph_db_without_views
+mkdir build && cd build
 cmake ..
 make -j32
 
-cd "$SCRIPT_DIR/tugraph_db_with_views"
-mkdir build & cd build
+cd ../tugraph_db_with_views
+mkdir build && cd build
 cmake ..
 make -j32
 
@@ -19,16 +19,16 @@ wget home.ustc.edu.cn/~angels/import_finbench_sf10.zip # finbenchSf10
 unzip import_snb_sf1.zip 
 unzip import_finbench_sf10.zip
 
-cd "$SCRIPT_DIR/import_snb_sf1"
+cd ./import_snb_sf1
 ../tugraph_db_without_views/build/output/lgraph_import --dir /data_wo_opt --verbose 2 -c import.conf -g ldbcSf1  --continue_on_error 1 --overwrite 1 --online false
 ../tugraph_db_with_views/build/output/lgraph_import --dir /data_opt --verbose 2 -c import.conf -g ldbcSf1  --continue_on_error 1 --overwrite 1 --online false
 
-cd "$SCRIPT_DIR/import_finbench_sf10"
+cd ../import_finbench_sf10
 ../tugraph_db_without_views/build/output/lgraph_import --dir /data_wo_opt --verbose 2 -c import.conf -g finbenchSf10  --continue_on_error 1 --overwrite 1 --online false --delimiter "|"
 ../tugraph_db_with_views/build/output/lgraph_import --dir /data_opt --verbose 2 -c import.conf -g finbenchSf10  --continue_on_error 1 --overwrite 1 --online false --delimiter "|"
 
 # start TuGraph
-cd "$SCRIPT_DIR/tugraph_db_without_views"
+cd ../tugraph_db_without_views
 ./build/output/lgraph_server -d start --directory /data_wo_opt --port 7071 --rpc_port 9091
-cd "$SCRIPT_DIR/tugraph_db_with_views"
+cd ../tugraph_db_with_views
 ./build/output/lgraph_server -d start --directory /data_opt --port 7072 --rpc_port 9092
